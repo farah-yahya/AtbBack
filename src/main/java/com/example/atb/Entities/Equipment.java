4eaeabea704f682a621f0bb5004bd7b77d129a4b
farah <farahyha79@gmail.com>
2024-04-23 19:18:05 +0100

    first commit

---
+++ src/main/java/com/example/atb/Entities/Equipment.java
@@ -0,0 +1,29 @@
+package com.example.atb.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Equipment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    String ref;
+    String name;
+    String category;
+    Date entree ;
+    Date sortie ;
+}
